c900db179f4d62190a864aee7c4c1ef419aa3ccb
ch1nya <chiniakinivan@gmail.com>
2023-07-24 19:59:58 +0800

    added sale banner and @worth to see@ block

--- src/components/App/Home.jsx
+++ src/components/App/Home.jsx
@@ -3,16 +3,19 @@ import Poster from '../Poster/Poster'
 import Products from '../Products/Products'
 import { useSelector } from 'react-redux'
 import s from '../../styles/Home.module.css'
+import Categories from '../Categories/Categories'
+import Banner from '../Banner/Banner'
 
 const Home = () => {
-const {list}  = useSelector(({products})=>products)
+const {products, categories}  = useSelector((state)=>state)
 
   return (
       <>
         <Poster />
-      
-      <Products  products={list} amount={5} title='Trending'/>
-    </>
+        <Products  products={products.list} amount={5} title='Trending'/>
+        <Categories  products={categories.list} amount={5} title='Worth to see'/>
+        <Banner />
+      </>
   )
 }
 
